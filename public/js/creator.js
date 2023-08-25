cc7971cbb7d77028a1b7bb3304a58307b7dc245e
Ziad Alfadl <alfadlziad@gmail.com>
2023-08-25 13:58:16 -0400

    character creator post function

--- public/js/creator.js
+++ public/js/creator.js
@@ -1,11 +1,27 @@
+const { response } = require("express");
 
-function newCharacterHandler(event) {
+const newCharacterHandler = async (event) => {
   event.preventDefault();
   const myFormData = new FormData(event.target);
   const formDataObj = {};
   
   myFormData.forEach((value, key) => (formDataObj[key] = value));
   console.log(formDataObj);
+
+  if (formDataObj) {
+    const response = await fetch('/api/create',  {
+      method: 'POST',
+      body: JSON.stringify(formDataObj),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    if (response.ok) {
+      console.log('ok')
+    } else {
+      console.log('not ok')
+    }
+  }
 }
 
 document.querySelector('form').addEventListener('submit',
